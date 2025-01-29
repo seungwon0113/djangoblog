2db9d7a0beacb4bd34768bf96b43277b40bd6dca
seungwon0113 <jungseungwon0113@gmail.com>
2025-01-29 12:15:56 +0900

    cors

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -58,9 +58,11 @@ INSTALLED_APPS = [
     "contacts",
     "likes",
     "tags",
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -167,6 +169,8 @@ CSRF_TRUSTED_ORIGINS = [
     "http://slog.my",
     "http://www.slog.my",
     "http://211.188.60.26",
+    "https://slog.my",
+    "https://www.slog.my",
 ]
 
 # Security Settings
@@ -179,5 +183,12 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 
 # CORS 설정
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://slog.my",
+    "http://www.slog.my",
+    "http://211.188.60.26",
+    "https://slog.my",
+    "https://www.slog.my",
+]
+
 CORS_ALLOW_CREDENTIALS = True
