57ddf77b3e3c60e6a645033eb657f385cd41d512
seungwon0113 <jungseungwon0113@gmail.com>
2025-01-29 15:38:00 +0900

    nginx

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -187,6 +187,19 @@ SESSION_COOKIE_SAMESITE = 'Lax'
 # CORS 설정
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://slog.my",
+    "http://www.slog.my",
+    "http://211.188.60.26",
+    "http://localhost",
+    "http://127.0.0.1",
+]
+
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^http://\w+\.slog\.my$",
+]
 
 CORS_ALLOW_METHODS = [
     'DELETE',
